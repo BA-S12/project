2a05e597641d6185e0c4eb78230bcfb3079a1486
MUSAB ALSHANQITI <151452396+musab14@users.noreply.github.com>
2024-04-26 19:00:37 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -14,18 +8,17 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 
-public class Investment extends Application {
+public class Project extends Application {
 
     private Design a = new Design();
 
     @Override
     public void start(Stage stage) throws IOException {
         a.stage = stage;
-        
+
         Image image = new Image("icon.jpg");
         //Line 1
         HBox inputPane = new HBox(a.lbl_initialBalanceTitle, a.tf_initialBalance, a.lbl_interestRateTitle,
@@ -56,24 +49,40 @@ public class Investment extends Application {
         HBox outputPane = new HBox(yearsPane, cyePane, totalBalancePane);
         outputPane.setAlignment(Pos.CENTER);
 
-        
 // Setup export button
         a.btn_export.setMaxWidth(Double.MAX_VALUE);
         a.btn_export.setOnAction(new ExportHandler());
 
 // Create root pane and arrange UI components
         BorderPane rootPane = new BorderPane();
-        
-        VBox top = new VBox(inputPane,messagePane, a.btn_calculate);
-        VBox bottom = new VBox(a.ta_history,a.btn_export);
+
+        VBox top = new VBox(inputPane, messagePane, a.btn_calculate);
+        VBox bottom = new VBox(a.ta_history, a.btn_export);
         a.ta_history.setDisable(true);
         //a.ta_history.addEventFilter(KeyEvent.KEY_TYPED, e -> e.consume());
-        
+
         rootPane.setTop(top);
         rootPane.setCenter(outputPane);
         rootPane.setBottom(bottom);
         BorderPane.setAlignment(outputPane, Pos.CENTER);
-        
+
+        //////////////////////////////////////////////
+        a.tf_initialBalance.setOnKeyPressed((e) -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                a.tf_interestRate.requestFocus();
+            }
+        });
+
+        a.tf_interestRate.setOnKeyPressed((e) -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                a.calculate();
+            } else if (e.getCode() == KeyCode.SPACE) {
+                if (!a.ta_history.getText().equalsIgnoreCase("")) {
+                    a.export();
+                }
+            }
+        });
+
 // Set padding for UI components
         Style.setPadding(8, a.lbl_initialBalanceTitle, a.lbl_interestRateTitle, a.tf_interestRate, a.tf_initialBalance,
                 a.lbl_years, a.lbl_yearHint, a.lbl_cye, a.lbl_cycHint, a.lbl_totalBalance, a.lbl_totalBalanceHint,
@@ -89,7 +98,7 @@ public class Investment extends Application {
 // Apply text styling to UI components
         Style.styleText("red", "bold", a.lbl_years, a.lbl_cye, a.lbl_totalBalance, a.lbl_message);
         Style.styleText("red", a.lbl_yearHint, a.lbl_cycHint, a.lbl_totalBalanceHint);
-        
+
 // Create and show the scene
         Scene scene = new Scene(rootPane);
         stage.setScene(scene);
@@ -102,6 +111,7 @@ public class Investment extends Application {
     }
 
     public class CalculateHandler implements EventHandler<ActionEvent> {
+
         @Override
         public void handle(ActionEvent event) {
             a.calculate();
@@ -109,6 +119,7 @@ public class Investment extends Application {
     }
 
     public class ExportHandler implements EventHandler<ActionEvent> {
+
         @Override
         public void handle(ActionEvent actionEvent) {
             a.export();
